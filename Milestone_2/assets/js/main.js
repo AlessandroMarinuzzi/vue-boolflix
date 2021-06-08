5be4fd476bf9c18469a6d113730eee51bbc45a47
Alessandro Marinuzzi <alessandromarinuzzi@gmail.com>
2021-06-08 14:55:25 +0100

    - Create getFlag function for flags displaying.

--- Milestone_2/assets/js/main.js
+++ Milestone_2/assets/js/main.js
@@ -13,12 +13,6 @@ const root = new Vue({
     methods : {
 
         search(){
-            // axios
-            // .get("https://api.themoviedb.org/3/search/movie?api_key=1bfdbda745d1e41948e58b036aa6ea62&query=" + this.query)
-            // .then(response => {
-            //     this.results = (response.data.results);
-            //     console.log(this.results);
-            // })
             const movieQuery = axios.get("https://api.themoviedb.org/3/search/movie?api_key=1bfdbda745d1e41948e58b036aa6ea62&query=" + this.query)
             const tvQuery = axios.get("https://api.themoviedb.org/3/search/tv?api_key=1bfdbda745d1e41948e58b036aa6ea62&query=" + this.query)
 
@@ -31,6 +25,12 @@ const root = new Vue({
                 this.tvResults = (response.data.results);
                 console.log(this.tvResults);
             })
+        },
+
+        getFlag(flagCode){
+            const flag = `https://www.countryflags.io/${flagCode}/shiny/32.png`;
+            console.log(flag);
+            return flag
         }
     },
 })
