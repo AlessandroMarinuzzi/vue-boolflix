ea144041ba9d7d229f03d56ee5505e67422af924
Alessandro Marinuzzi <alessandromarinuzzi@gmail.com>
2021-06-08 13:51:42 +0100

    - Add tvShow_list to display tvShows too in a different section.

--- Milestone_2/assets/js/main.js
+++ Milestone_2/assets/js/main.js
@@ -5,20 +5,47 @@ const root = new Vue({
 
         query: "",
 
-        results:{
+        movieResults:{},
 
-        },
+        tvResults:{}
     },
 
     methods : {
 
         search(){
-            axios
-            .get("https://api.themoviedb.org/3/search/movie?api_key=1bfdbda745d1e41948e58b036aa6ea62&query=" + this.query)
-            .then(response => {
-                this.results = (response.data.results);
-                console.log(this.results);
+            // axios
+            // .get("https://api.themoviedb.org/3/search/movie?api_key=1bfdbda745d1e41948e58b036aa6ea62&query=" + this.query)
+            // .then(response => {
+            //     this.results = (response.data.results);
+            //     console.log(this.results);
+            // })
+            const movieQuery = axios.get("https://api.themoviedb.org/3/search/movie?api_key=1bfdbda745d1e41948e58b036aa6ea62&query=" + this.query)
+            const tvQuery = axios.get("https://api.themoviedb.org/3/search/tv?api_key=1bfdbda745d1e41948e58b036aa6ea62&query=" + this.query)
+
+            movieQuery.then(response =>{
+                this.movieResults = (response.data.results);
+                console.log(this.movieResults);
+            })
+            
+            tvQuery.then(response =>{
+                this.tvResults = (response.data.results);
+                console.log(this.tvResults);
             })
         }
     },
 })
+
+
+
+
+
+
+
+// hhhhhhhhhhhhhhhhhhhh
+// axios.all([movieQuery, tvQuery])
+// .then(axios.spread((...responses) => {
+//     const movieResults = responses[0].data.results;
+//     const tvResults = responses[1].data.results;
+
+//     this.results = [...movieResults, ...tvResults];
+// }))
